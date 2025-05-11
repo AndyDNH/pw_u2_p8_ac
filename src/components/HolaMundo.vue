b6c977bd31ef16d1ab016cc328c74afeb38d2f5d
Andres Cueva <132870444+AndyDNH@users.noreply.github.com>
2025-05-10 20:49:14 -0500

    Taller 12
    Hola Mundo Vue

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="titulo">Universidad Central del Ecuador</h1>
+    <h2>Facultad de Ingenieria</h2>
+    <h3>Carrera de Computacion</h3>
+    <h4>Hola Mundo desde Vue</h4>
+    <p>Andres Cueva</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.titulo {
+    color: red;
+}
+h2 {
+    background: blue;
+}
+
+p {
+    color: green;
+    background: blueviolet;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 40px;
+}
+
+</style>
